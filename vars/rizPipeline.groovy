3adfb33830598a712d2e129c55ca0db6d66bdcca
rizkysyafrullah <rizkysyafrullah@gmail.com>
2023-03-04 08:07:03 +0700

    add rizPipeline

---
+++ vars/rizPipeline.groovy
@@ -0,0 +1,20 @@
+def call(Map config){
+
+    if(config.type = "maven"){
+        mavenPipeline(;)
+    } else {
+        pipeline {
+        agent any 
+        stages {    
+            stage("Pipeline tidak didukung") {
+                steps {
+                    script {
+                        echo("Pipeline tidak didukung")
+                    }
+                }
+            } 
+        }
+        }
+    }
+
+}
