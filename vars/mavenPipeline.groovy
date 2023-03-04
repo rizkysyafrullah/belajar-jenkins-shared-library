80fd09144b8281a8194457ed1046a3fc2558df33
rizkysyafrullah <rizkysyafrullah@gmail.com>
2023-03-04 07:41:09 +0700

    add mavenPipeline.groovy

---
+++ vars/mavenPipeline.groovy
@@ -0,0 +1,34 @@
+def Panggil() {
+pipeline {
+  agent any 
+  stages {
+
+    
+    stage("Maven Clean") {
+      steps {
+        script {
+          maven(["clean"])
+        }
+      }
+    }
+
+    stage("Maven Compile") {
+      steps {
+        script {
+          maven(["compile"])
+        }
+      }
+    }
+
+    stage("Maven Test") {
+      steps {
+        script {
+          maven(["test"])
+        }
+      }
+    }
+
+
+  }
+}
+}
